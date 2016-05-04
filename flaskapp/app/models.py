2b57b92bff5916c16acbf52b9e035ebf2592b76b
Paul Tan <tpaul1203@gmail.com>
2016-05-04 06:39:16 -0400

    Added more to Flask

--- flaskapp/app/models.py
+++ flaskapp/app/models.py
@@ -2,95 +2,110 @@ import sqlite3 as sql
 import json
 import collections
 
+
 def add_user(username, password):
-	with sql.connect("database.db") as con:
-		cur = con.cursor()
-		cur.execute("INSERT INTO users VALUES (null,?,?)", (username, password))
-		con.commit()
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO users VALUES (null,?,?)", (username, password))
+        con.commit()
+
 
 def add_meetup(classname, subject, starttime, endtime, createdby):
-	 with sql.connect("database.db") as con:
-		cur = con.cursor()
-		cur.execute("INSERT INTO meetups VALUES (null,?,?,?,?,?)", (classname, subject, starttime, endtime, createdby))
-		con.commit()
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO meetups VALUES (null,?,?,?,?,?)", (classname, subject, starttime, endtime, createdby))
+        con.commit()
+
 
 def set_going(uid, eid):
-	with sql.connect("database.db") as con:
-		cur = con.cursor()
-		cur.execute("INSERT INTO RSVPS VALUES (?,?)", (eid, uid))
-		con.commit()
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO RSVPS VALUES (?,?)", (eid, uid))
+        con.commit()
+
 
 def is_going(uid, eid):
-	with sql.connect("database.db") as con:
-		cur = con.cursor()
-		cur.execute("SELECT * FROM RSVPS WHERE uid = ? AND eid = ?", (uid, eid))
-		if not(cur.fetchall()):
-			return False
-		else:
-			return True
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM RSVPS WHERE uid = ? AND eid = ?", (uid, eid))
+        if not (cur.fetchall()):
+            return False
+        else:
+            return True
+
 
 def get_all_meetups():
-	with sql.connect("database.db") as con:
-		cur = con.cursor()
-		cur.execute("SELECT * FROM meetups;")
-		rs = list(cur.fetchall())
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM meetups;")
+        rs = list(cur.fetchall())
 
-		meetuplist = []
-		for row in rs:
-			mdict = collections.OrderedDict()
-			mdict['eid'] = row[0]
-			mdict['starttime'] = row[1]
-			mdict['endtime'] = row[2]
-			mdict['classname'] = row[3]
-			mdict['subject'] = row[4]
-			mdict['createdby'] = row[5]
+        meetuplist = []
+        for row in rs:
+            mdict = collections.OrderedDict()
+            mdict['eid'] = row[0]
+            mdict['starttime'] = row[1]
+            mdict['endtime'] = row[2]
+            mdict['classname'] = row[3]
+            mdict['subject'] = row[4]
+            mdict['createdby'] = row[5]
 
-			meetuplist.append(mdict)
+            meetuplist.append(mdict)
 
+        return (json.dumps(meetuplist))
 
-		return(json.dumps(meetuplist))
 
 def get_all_attendees(eid):
-	with sql.connect("database.db") as con:
-		cur = con.cursor()
-		cur.execute("SELECT * FROM RSVPS WHERE eid = ?;",eid)
-		rs = list(cur.fetchall())
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM RSVPS WHERE eid = ?;", eid)
+        rs = list(cur.fetchall())
 
-		listofattendees = []
-		for row in rs:
-			listofattendees.append(getUsernameFromUID(row[1])) #change to getUsername(uid)
+        listofattendees = []
+        for row in rs:
+            listofattendees.append(getUsernameFromUID(row[1]))  # change to getUsername(uid)
 
-		return (json.dumps(listofattendees))
+        return (json.dumps(listofattendees))
+
 
 def getUsernameFromUID(uid):
-	with sql.connect("database.db") as con:
-		cur = con.cursor()
-		cur.execute("SELECT username FROM users WHERE uid = ?;",str(uid))
-		rs = cur.fetchall()
-		if not rs:
-			return "Error"
-		else:
-			return rs[0][0]
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute("SELECT username FROM users WHERE uid = ?;", str(uid))
+        rs = cur.fetchall()
+        if not rs:
+            return "Error"
+        else:
+            return rs[0][0]
 
 
 def get_meetup_info(eid):
-	with sql.connect("database.db") as con:
-		cur = con.cursor()
-		cur.execute("SELECT * FROM meetups where eid = ?;", str(eid))
-		row = cur.fetchone()
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM meetups where eid = ?;", str(eid))
+        row = cur.fetchone()
 
-		mdict = collections.OrderedDict()
-		mdict['eid'] = row[0]
-		mdict['starttime'] = row[1]
-		mdict['endtime'] = row[2]
-		mdict['classname'] = row[3]
-		mdict['subject'] = row[4]
-		mdict['createdby'] = row[5]
+        mdict = collections.OrderedDict()
+        mdict['eid'] = row[0]
+        mdict['starttime'] = row[1]
+        mdict['endtime'] = row[2]
+        mdict['classname'] = row[3]
+        mdict['subject'] = row[4]
+        mdict['createdby'] = row[5]
+
+        return (json.dumps(mdict))
 
 
-		return(json.dumps(mdict))
+def confirmUserPass(user,passw):
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute("SELECT password FROM users where username = ?;", user)
+        row = cur.fetchall()
 
-
-#def exists_user():
-#def delete_meetup():
-#def delete_user():
+        if(str(user) == row[1] and str(passw) == row[2]):
+            return True
+        else:
+            return False
+        # def exists_user():
+        # def delete_meetup():
+        # def delete_user():
--- flaskapp/app/views.py
+++ flaskapp/app/views.py
@@ -1,78 +1,120 @@
 from app import app
-from flask import render_template,redirect, request, flash,g,session,url_for
+from flask import render_template, redirect, request, flash, g, session, url_for, send_file
 from models import *
 
-@app.route('/', methods = ["GET", "POST"])
-@app.route('/index', methods = ["GET," "POST"])
-def index():
-	return render_template("index.html")
-	#return get_all_meetups()
-	#return get_all_attendees('2')
-	#return get_meetup_info(2)
+app.secret_key = "donttellanyonemysecret"
 
+# First Button: Homepage
+@app.route('/', methods=["GET", "POST"])
+@app.route('/index', methods=["GET", "POST"])
+def index():
+    if 'username' in session:
+        return render_template("index.html",username =session['username'])
+    else:
+        return render_template("index.html")
+
+# return get_all_meetups()
+# return get_all_attendees('2')
+# return get_meetup_info(2)
+
+@app.route("/css/<css>")
+def style_render(css):
+    return render_template("css/%s"%css)
+
+@app.route("/images/<image>")
+def image_render(image):
+    filename = ('templates\\images\\%s'%image)
+    return send_file(filename,mimetype='image/jpeg')
+
+@app.route("/js/<js>")
+def js_render(js):
+    return render_template("js/%s"%js)
+
+# Second Button
+@app.route("/meetup", methods=["GET", "POST"])
+def meetup():
+    return render_template("meetup.html")
+
+@app.route("/createmeetup", methods=["GET", "POST"])
+def createmeetup():
+    starttime = request.form['starttime']
+    endtime = request.form['endtime']
+    classname = request.form['classname']
+    subject = request.form['subject']
+    createdby = request.form['createdby']
+    add_meetup(starttime, endtime, classname, subject, createdby)
+    return render_template("createdmeetup.html")
+
+# Third Button
+@app.route("/attend", methods=["GET", "POST"])
+def attend():
+    return render_template("attend.html")
+
+@app.route("/doRSVP", methods=["GET", "POST"])
+def doRSVP():
+    uid = request.form['uid']
+    eid = request.form['eid']
+    set_going(uid, eid)
+    return eid
+
+# Fourth Button
+@app.route("/checkattendance", methods=["GET", "POST"])
+def checkattendance():
+    return render_template("checkattendance.html")
+
+
+# Fifth Button
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
+
+@app.route("/signedup", methods=["GET", "POST"])
+def signedup():
+    username = request.form['username']
+    password = request.form['password']
+    session['username'] = username
+    session['logged_in'] = True
+    add_user(username, password)
+    return render_template("signedup.html")
+
+# Sixth Button
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if 'username' in session:
+        return render_template("login.html",username=session['username'])
+    else:
+        return render_template("login.html")
+
+@app.route("/loggedin", methods=["GET","POST"])
+def loggedin():
+    logUser = request.form['username']
+    logPass = request.form['password']
+    loginStatus = False
+    loginStatus = confirmUserPass(logUser,logPass)
+    if loginStatus == True:
+        session['username'] = login_user
+        return render_template("loggedin.html",username=login_user)
+    else:
+        return render_template("loggedin.html",error="Bad user or password")
+
+
+@app.route("/getattend", methods=["GET", "POST"])
+def getattend():
+    uid = request.form['uid']
+    eid = request.form['eid']
+    return str(is_going(uid, eid))
 
 @app.route("/getallmeetups")
 def getallmeetups():
-	return get_all_meetups()
+    return get_all_meetups()
 
-@app.route("/signup", methods=["GET","POST"])
-def signup():
-	return render_template("signup.html")
-
-@app.route("/signedup", methods=["GET","POST"])
-def signedup():
-	username = request.form['username']
-	password = request.form['password']
-
-	add_user(username, password)
-
-	return (username)
-
-@app.route("/meetup", methods = ["GET", "POST"])
-def meetup():
-	return render_template("meetup.html")
-
-@app.route("/createmeetup", methods = ["GET", "POST"])
-def createmeetup():
-	starttime = request.form['starttime']
-	endtime = request.form['endtime']
-	classname = request.form['classname']
-	subject = request.form['subject']
-	createdby = request.form['createdby']
-
-	add_meetup(starttime, endtime, classname, subject, createdby)
-
-	return(starttime)
-
-@app.route("/attend", methods = ["GET", "POST"])
-def attend():
-	return render_template("attend.html")
-
-@app.route("/doRSVP", methods = ["GET", "POST"])
-def doRSVP():
-	uid = request.form['uid']
-	eid = request.form['eid']
-
-	set_going(uid, eid)
-	return eid
-
-@app.route("/checkattendance", methods = ["GET", "POST"])
-def checkattendance():
-	return render_template("checkattendance.html")
-
-@app.route("/getattend", methods = ["GET", "POST"])
-def getattend():
-	uid = request.form['uid']
-	eid = request.form['eid']
-
-	return str(is_going(uid, eid))
-
-
-@app.route("/minfo", methods = ["GET", "POST"])
+@app.route("/minfo", methods=["GET", "POST"])
 def minfo():
-	return render_template("getmeetupinfo.html")
+    return render_template("getmeetupinfo.html")
+
 
 @app.route("/_minfo")
 def _minfo():
-	a = request.args.get('a', 0, type=int)
-	return get_meetup_info(a)
+    a = request.args.get('a', 0, type=int)
+    return get_meetup_info(a)
+
